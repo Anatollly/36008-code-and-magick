a15514b44e0e8179ffd0734a8ae96204496c8ab8
nkirdis <nkirdis@list.ru>
2016-08-04 21:40:14 +0300

    [code-and-magick]: Добавлена функция getMessage для вывода сообщений со статусом игры на экран

---
+++ build/js/check.js
@@ -0,0 +1,30 @@
+var getMessage = function (a, b) {
+  var message;
+  var sum = 0;
+  var length = 0;
+
+  if (typeof a == 'boolean') {
+    if (a) {
+      message = 'Я попал в ' + b;
+    } else {
+      message = 'Я никуда не попал';
+    }
+
+  } else if (typeof a == 'number') {
+    message = 'Я прыгнул на ' + a * 100 + ' сантиметров';
+
+  } else if (typeof a == 'object') {
+    if (typeof b == 'object') {
+      for (i = 0; i < a.length; i++) {
+        length += a[i] * b[i];
+      }
+      message = 'Я прошёл ' + length + ' метров';
+    } else {
+      for (i = 0; i < a.length; i++) {
+        sum += a[i];
+      }
+      message = 'Я прошёл ' + sum + ' шагов';
+    }
+  }
+  return message;
+}
